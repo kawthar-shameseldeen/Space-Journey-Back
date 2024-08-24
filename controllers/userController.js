3831b901b2b949b440a77ae853be0c5aea5b5d40
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:52:04 +0300

    added createUser method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,11 @@
 import { User } from "../models/userModel.js";
+export const createUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    const user = new User(userData);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+};
