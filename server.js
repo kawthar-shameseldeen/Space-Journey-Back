a5f72ebb8596292b139af9e5ed89f1d3b77c2026
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:02:45 +0300

    imported websocket server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 import express from "express";
 import { databaseConnection } from "./database/connection.js";
 import dotenv from "dotenv";
+import http from "http";
+import { WebSocketServer } from "ws";
