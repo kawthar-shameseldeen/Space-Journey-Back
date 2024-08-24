135f9a7d845955b70df0573f2c61db73c6e6a88d
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 18:52:54 +0300

    added db connection and name

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
 export const databaseConnection = async()=>{
+    try {
+        await mongoose.connect(process.env.DATABASE_URL, {
+          dbName: "Space", 
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
     
+        console.log("Connected to database");
+      
+      } catch (error) {
+        console.log(error);
+    
+        console.log("Failed to connect to database");
+      }
 }
