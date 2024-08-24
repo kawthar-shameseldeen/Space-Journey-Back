7525f9082015e0525a9c58cf96322c8830873ff0
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 20:06:41 +0300

    added routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import {
-    getAllUsers,
-    getUserByUsername,
-    createUser,
-    registerIot,
-  } from "../controllers/userControlller.js";
-  const router = new Router();
+  getAllUsers,
+  getUserByUsername,
+  createUser,
+  registerIot,
+} from "../controllers/userControlller.js";
+const router = new Router();
+router.get("/all", getAllUsers);
+router.get("/:username", getUserByUsername);
+router.post("/create", createUser);
+router.post("/iot", registerIot);
