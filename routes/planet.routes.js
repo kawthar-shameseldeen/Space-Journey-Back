76f59721d3760777c83eeaa54e5ad515215bd6f1
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 20:09:20 +0300

    added some more routes

--- routes/planet.routes.js
+++ routes/planet.routes.js
@@ -12,4 +12,7 @@ const router = new Router();
 router.post("/planets", createPlanet);
 router.get("/planets", getAllPlanets);
 router.get('/planets/:name', getPlanetByName);
+router.post("/planets/display", displayPlanet);
+router.put("/planets/:id", updatePlanet);
+router.delete("/planets/:id", deletePlanet);
 
