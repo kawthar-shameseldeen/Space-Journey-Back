6d6de1de9c2ef3b47ee5bdd7f2767aac9b1e0258
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 20:08:02 +0300

    imported planetController

--- routes/planet.routes.js
+++ routes/planet.routes.js
@@ -1,1 +1,10 @@
 import { Router } from "express";
+import {
+    createPlanet,
+    getAllPlanets,
+    getPlanetByName,
+    displayPlanet,
+    updatePlanet,
+    deletePlanet,
+  } from "../controllers/palanetController.js";
+  
