6016402d43b1b4ea7362e31eb3cad525d0dca93f
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:53:16 +0300

    added getUserByUserName method

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,3 +17,17 @@ export const getAllUsers = async (req, res) => {
       res.status(500).json({ message: "Error retrieving users", error });
     }
   };
+  
+export const getUserByUsername = async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.params.username }).select(
+        "-password +iot"
+      ); 
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Error retrieving user", error });
+    }
+  };
