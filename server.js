f0db04ab6ca3317b0914ab5239b28c644db21462
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:06:28 +0300

    added user and planet routes

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ import userRoutes from "./routes/user.routes.js";
 import planetRoutes from "./routes/planet.routes.js";
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/user", userRoutes);
+app.use("/api", planetRoutes);
