d530d8373254601dc372b0c6bf8876a4ac470e75
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:06:03 +0300

    imported  userroutes and planetroutes

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ import { databaseConnection } from "./database/connection.js";
 import dotenv from "dotenv";
 import http from "http";
 import { WebSocketServer } from "ws";
+import userRoutes from "./routes/user.routes.js";
+import planetRoutes from "./routes/planet.routes.js";
+
+dotenv.config();
