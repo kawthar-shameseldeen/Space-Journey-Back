a0546e8ad5cfe43e36521c08aaec63ccc6191a8b
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:41:29 +0300

    addded bcrypt function

--- models/userModel.js
+++ models/userModel.js
@@ -38,3 +38,19 @@ const userSchema = new Schema({
     default: Date.now,
   },
 });
+
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+  
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
