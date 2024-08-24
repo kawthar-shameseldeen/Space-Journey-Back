7e287db553c8e521de432aa4894097bcd5fb78e2
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-24 19:55:20 +0300

    added createPlanet method

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,10 @@
 import { Planet } from "../models/planetModel.js";
+export const createPlanet = async (req, res) => {
+    try {
+      const planet = new Planet(req.body);
+      await planet.save();
+      res.status(201).json(planet);
+    } catch (error) {
+      res.status(500).json({ message: "Error creating planet", error });
+    }
+  };
