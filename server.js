214ead26801e8f5c0fab59a10a7cda39b6083f92
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-25 17:58:12 +0300

    added cors in server.js

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ import { WebSocketServer } from "ws";
 import userRoutes from "./routes/user.routes.js";
 import planetRoutes from "./routes/planet.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/user", userRoutes);
