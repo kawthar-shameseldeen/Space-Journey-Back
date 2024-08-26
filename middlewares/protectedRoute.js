71ff57c6519db13da693dd3cbe1cec234914f26a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 20:15:39 +0300

    added variables

--- middlewares/protectedRoute.js
+++ middlewares/protectedRoute.js
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 import {User} from "../models/userModel.js";
 
 const protect = async (req, res, next) => {
- 
+  let token;
+
 };
+
 
 export { protect, adminProtect };
