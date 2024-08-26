284b47fe2c8601d1849dc97abffc0615e1c18409
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 12:13:27 +0300

    fixed bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ export const createUser = async (req, res) => {
 };
 export const getAllUsers = async (req, res) => {
     try {
-      const users = await User.find().select("-password +iot");
+      const users = await User.find({},"name email role iotDevices");
       res.status(200).json(users);
     } catch (error) {
       res.status(500).json({ message: "Error retrieving users", error });
