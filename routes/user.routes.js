003b1f77e10b440279312400d72c9b1f23dde99b
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-26 20:12:25 +0300

    added protected routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,9 +5,10 @@ import {
   createUser,
   registerIot,
 } from "../controllers/userController.js";
+import { adminProtect,protect } from "../middlewares/protectedRoute.js";
 const router = new Router();
-router.get("/all", getAllUsers);
-router.get("/:username", getUserByUsername);
+router.get("/all",adminProtect , getAllUsers);
+router.get("/:username", adminProtect,getUserByUsername);
 router.post("/create", createUser);
 router.post("/iot", registerIot);
 
