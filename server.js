f146e1689c4647b160bc0b3060b3be6ce136b4aa
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-27 12:08:02 +0300

    tested websockets

--- server.js
+++ server.js
@@ -7,37 +7,37 @@ import userRoutes from "./routes/user.routes.js";
 import planetRoutes from "./routes/planet.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
+
 app.use("/api/user", userRoutes);
 app.use("/api", planetRoutes);
 app.use("/api", authRouter);
 
-
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
-wss.on("connection", (ws, req) => {
+
+wss.on("connection", (ws) => {
     console.log("New WebSocket connection established");
-  
+
     ws.on("message", (message) => {
-      console.log("Received message:", message);
-  
-      ws.send(`Server received: ${message}`);
+        console.log("Received message:", message);
+        ws.send(`Server received: ${message}`);
     });
-  
+
     ws.on("close", () => {
-      console.log("WebSocket connection closed");
+        console.log("WebSocket connection closed");
     });
-  });
-  
-  const PORT = process.env.SERVER_PORT || 8080;
-  server.listen(PORT, () => {
+});
+
+const PORT = process.env.SERVER_PORT || 8080;
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  
     databaseConnection();
-  });
-  
+
+ 
+});
