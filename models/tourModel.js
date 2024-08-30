3aa926f855ff43f318b802773cd93edd99e2fedc
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 12:14:28 +0300

    added current position attribute

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 const tourSchema = new Schema({
     
+    currentPosition: {
+           
+          },
+        
 })
 export  const Tour = mongoose.model("Tour", tourSchema)
