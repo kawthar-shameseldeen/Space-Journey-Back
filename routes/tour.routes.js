5c0e3b415afdd93010bdc4b30217e48779ece365
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 13:37:18 +0300

    added routes

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createTour, getAllPositions, getTourByName, displayTour, updateTour, deleteTour } from "../controllers/tourController.js";
+import { createTour, getAllPositions, getPosition, displayTour, updateTour, deleteTour } from "../controllers/tourController.js";
 
 const router=new Router();
 router.post("/tour",createTour);
 router.get("/tour",getAllPositions);
+router.get('/tour/:id',getPosition);
