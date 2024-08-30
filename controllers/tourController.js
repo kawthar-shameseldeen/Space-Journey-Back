3a06c86a41710da13f06f1f466fc9d9a977dae15
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-30 12:29:51 +0300

    saved tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@ import {Tour} from "../models/tourModel.js";
 export const createTour =async()=>{
     try{
         const tour =new Tour(req.body);
+        await tour.save();
 
     }
     catch(error){
