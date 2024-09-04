51cfb96ea39b4b61d867bb1443bc844d43744ab7
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 11:58:00 +0300

    added tourid to login

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,6 +78,7 @@ export const login = async (req, res) => {
         username: user.username,
         email: user.email,
         role: user.role,
+        tourId: user.tourId,
       },
     });
   } catch (error) {
