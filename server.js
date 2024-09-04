1bc07da9ad42274d11a902025788b47d098dad99
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 15:15:12 +0300

    changed in the websocket server

--- server.js
+++ server.js
@@ -6,42 +6,62 @@ import { WebSocketServer } from "ws";
 import userRoutes from "./routes/user.routes.js";
 import planetRoutes from "./routes/planet.routes.js";
 import authRouter from "./routes/auth.routes.js";
-import tourRouter from "./routes/tour.routes.js"
-import cors from 'cors';
+import tourRouter from "./routes/tour.routes.js";
+import cors from "cors";
+import { Tour } from "./models/tourModel.js"; 
 
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+  app.use(cors(corsOptions));
+  
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api", planetRoutes);
 app.use("/api", authRouter);
-app.use('/api',tourRouter);
+app.use("/api", tourRouter);
+
 
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-wss.on("connection", (ws) => {
+
+const connections = {};
+wss.on('connection', (ws) => {
     console.log("New WebSocket connection established");
-
-    ws.on("message", (message) => {
-        console.log("Received message:", message);
-        ws.send(`Server received: ${message}`);
+  
+    ws.on('message', (message) => {
+      try {
+        const parsedMessage = JSON.parse(message);
+        console.log("Received message from client:", parsedMessage);
+  
+        if (parsedMessage.planetName) {
+          console.log(`Planet hovered: ${parsedMessage.planetName}`);
+        
+        }
+      } catch (error) {
+        console.error("Error parsing WebSocket message:", error);
+      }
     });
-
-    ws.on("close", () => {
-        console.log("WebSocket connection closed");
+  
+    ws.on('close', () => {
+      console.log("WebSocket connection closed");
     });
-});
+  });
 
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.SERVER_PORT ;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    databaseConnection();
-
- 
+  console.log(`Server running on port ${PORT}`);
+  databaseConnection();
 });
+
 export { wss };
