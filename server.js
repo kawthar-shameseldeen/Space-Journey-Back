0b3231ab391adb261658d2b578a3eb81d71e536a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-04 20:25:06 +0300

    added client to ws

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ import tourRouter from "./routes/tour.routes.js";
 import cors from "cors";
 import { Tour } from "./models/tourModel.js"; 
 
-
 dotenv.config();
 
 const app = express();
@@ -30,35 +29,48 @@ app.use("/api", planetRoutes);
 app.use("/api", authRouter);
 app.use("/api", tourRouter);
 
-
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
 
-const connections = {};
+const clients = [];
+
 wss.on('connection', (ws) => {
     console.log("New WebSocket connection established");
-  
+
+    clients.push(ws);
+
     ws.on('message', (message) => {
       try {
         const parsedMessage = JSON.parse(message);
         console.log("Received message from client:", parsedMessage);
-  
+
         if (parsedMessage.planetName) {
           console.log(`Planet hovered: ${parsedMessage.planetName}`);
-        
+
+         
+          clients.forEach((client) => {
+            if (client.readyState === ws.OPEN) {
+              client.send(JSON.stringify({ planetName: parsedMessage.planetName }));
+            }
+          });
         }
       } catch (error) {
         console.error("Error parsing WebSocket message:", error);
       }
     });
-  
+
     ws.on('close', () => {
       console.log("WebSocket connection closed");
+    
+      const index = clients.indexOf(ws);
+      if (index !== -1) {
+        clients.splice(index, 1);
+      }
     });
-  });
+});
 
-const PORT = process.env.SERVER_PORT ;
+const PORT = process.env.SERVER_PORT;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   databaseConnection();
