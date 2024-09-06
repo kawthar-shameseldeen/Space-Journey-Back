3e8957ac856b34bf46ba49dbf71cbbc085194dae
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-06 18:15:48 +0300

    added createEvent

--- controllers/eventControlelr.js
+++ controllers/eventControlelr.js
@@ -7,3 +7,14 @@ export const getEvents = async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch events', error });
   }
 };
+
+export const createEvent = async (req, res) => {
+  const { name, description, date, liveStreamUrl } = req.body;
+  try {
+    const event = new Event({ name, description, date, liveStreamUrl });
+    await event.save();
+    res.status(201).json(event);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to create event', error });
+  }
+};
