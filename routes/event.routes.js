99f5e1c22aea6f08fc8b81b25b57ff2b308623ce
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-06 18:16:20 +0300

    added routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,1 +1,9 @@
 import express from 'express';
+import { getEvents, createEvent } from '../controllers/eventControlelr.js';
+
+const router = express.Router();
+
+router.get('/events', getEvents);     
+router.post('/events', createEvent);   
+
+export default router;
