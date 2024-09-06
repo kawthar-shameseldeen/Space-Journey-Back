4172108b28c7fac839ad323966e0d4cc5b963a30
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-09-06 18:14:27 +0300

    added attributes

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
 
 const eventSchema = new mongoose.Schema({
-
+  name: { type: String, required: true },
+  description: { type: String },
+  date: { type: Date, required: true },
+  liveStreamUrl: { type: String },
+  notificationSent: { type: Boolean, default: false },
 });
 
 const Event = mongoose.model('Event', eventSchema);
